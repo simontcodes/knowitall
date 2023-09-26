b39c4f6722baeddb11b264a3aa9769df3e286122
simon codes <simont.codes@gmail.com>
2023-09-26 14:31:47 -0400

    added /play/mcq/:id and /play/open-ended/:id routes, added MCQ component that displays the questions and its options

---
+++ app/play/mcq/[gameId]/page.tsx
@@ -0,0 +1,44 @@
+import MCQ from "@/components/MCQ";
+import { prisma } from "@/lib/db";
+import { getAuthSession } from "@/lib/nextauth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: {
+    gameId: string;
+  };
+};
+
+const McqPage = async ({ params: { gameId } }: Props) => {
+  const session = await getAuthSession();
+  //route protection
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  //get the game and its questions from db
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
+          // select is being use to leave out the answer field so the front end doesnt know the asnwer to the question
+        },
+      },
+    },
+  });
+
+  if (!game || game.gameType !== "mcq") {
+    return redirect("/quiz");
+  }
+
+  return <MCQ game={game} />;
+};
+
+export default McqPage;
---
+++ app/play/open-ended/[gameId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const OpenEndedPage = (props: Props) => {
+  return <div>Open Ended questions page</div>;
+};
+
+export default OpenEndedPage;
